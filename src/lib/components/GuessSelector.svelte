89e4996329191d7d4b242a62a279d335e0b63376
Chrizpy <christofferhaj@gmail.com>
2026-02-06 13:46:49 +0100

    fix a11y issues

--- src/lib/components/GuessSelector.svelte
+++ src/lib/components/GuessSelector.svelte
@@ -80,7 +80,9 @@
   $: playersWithCards = players.filter(p => p.discardPile.length > 0);
 </script>
 
+<!-- svelte-ignore a11y_click_events_have_key_events a11y_no_static_element_interactions -->
 <div class="guess-selector-overlay" role="dialog" aria-modal="true" tabindex="0" on:click={showCancel ? onCancel : undefined} on:keydown={(e) => e.key === 'Escape' && showCancel && onCancel()}>
+  <!-- svelte-ignore a11y_click_events_have_key_events a11y_no_static_element_interactions -->
   <div class="guess-selector" class:revenge-style={headerStyle === 'revenge'} on:click|stopPropagation>
     {#if headerIcon}
       <div class="header-icon">{headerIcon}</div>
